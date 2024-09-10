c72494121e17c87e792e02fd29ee20496f0e8318
kcjdg <kencrisjohn@gmail.com>
2024-09-10 15:00:46 +0800

    Change name

--- voter-profile/src/main/java/ph/dgtech/halalan/voter/profile/config/SwaggerConfig.java
+++ voter-profile/src/main/java/ph/dgtech/halalan/voter/profile/config/SwaggerConfig.java
@@ -8,8 +8,8 @@ import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
         info = @Info(
-                title = "Product Service API",
-                description = "Product API documentation",
+                title = "Voter Profile API",
+                description = "Voter Profile API documentation",
                 version = "1.0"
         ),
         security = @SecurityRequirement(name = "oauth2_bearer"),
