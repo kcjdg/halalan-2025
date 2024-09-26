a91a0fa8ff1aabde880bd7497edc5336d44eaf8a
kcjdg <kencrisjohn@gmail.com>
2024-09-26 20:13:49 +0800

    feature/polling-service: Add @NamedStoredProcedure and @SqlResultSetMapping for mapping of fields

--- polling-service/src/main/java/ph/dgtech/halalan/polling/model/location/Region.java
+++ polling-service/src/main/java/ph/dgtech/halalan/polling/model/location/Region.java
@@ -2,11 +2,37 @@ package ph.dgtech.halalan.polling.model.location;
 
 import jakarta.persistence.*;
 import lombok.*;
-import ph.dgtech.halalan.polling.dto.AddressDetails;
+import ph.dgtech.halalan.polling.dto.address.AddressDetails;
 
 import java.util.Set;
 
 @Entity
+
+@SqlResultSetMapping(
+        name = "AddressDetailsMapping",
+        classes = @ConstructorResult(
+                targetClass = AddressDetails.class,
+                columns = {
+                        @ColumnResult(name = "region", type = String.class),
+                        @ColumnResult(name = "province", type = String.class),
+                        @ColumnResult(name = "municipality", type = String.class),
+                        @ColumnResult(name = "barangay", type = String.class)
+                }
+        )
+)
+@NamedStoredProcedureQuery(
+        name = "getLocationDetails",
+        procedureName = "GetAddressDetails",
+        resultSetMappings = "AddressDetailsMapping",
+        parameters = {
+                @StoredProcedureParameter(mode = ParameterMode.IN, name = "regionName", type = String.class),
+                @StoredProcedureParameter(mode = ParameterMode.IN, name = "provinceName", type = String.class),
+                @StoredProcedureParameter(mode = ParameterMode.IN, name = "municipalityName", type = String.class),
+                @StoredProcedureParameter(mode = ParameterMode.IN, name = "barangayName", type = String.class)
+        }
+)
+
+
 @Table(name = "t_region")
 @Data
 public class Region {
