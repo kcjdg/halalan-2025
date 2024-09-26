bc1b44daa40a57b686cff3ba8318573d1bfe2a78
kcjdg <kencrisjohn@gmail.com>
2024-09-26 20:14:14 +0800

    feature/polling-service: change mapping

--- polling-service/src/main/java/ph/dgtech/halalan/polling/repository/location/AddressValidationRepository.java
+++ polling-service/src/main/java/ph/dgtech/halalan/polling/repository/location/AddressValidationRepository.java
@@ -1,22 +1,19 @@
 package ph.dgtech.halalan.polling.repository.location;
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.query.Procedure;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import ph.dgtech.halalan.polling.dto.address.AddressDetails;
 import ph.dgtech.halalan.polling.model.location.Region;
 
-import java.util.List;
-
 @Repository
-public interface AddressValidationRepository extends CrudRepository<Region, Long> {
+public interface AddressValidationRepository extends JpaRepository<Region, Long> {
 
-        @Procedure(value = "GetLocationDetails", outputParameterName = "addressDetailsMapping")
-        List<Object[]> getLocationDetails(
-                @Param("regionName") String regionName,
-                @Param("provinceName") String provinceName,
-                @Param("municipalityName") String municipalityName,
-                @Param("barangayName") String barangayName
-        );
+    @Procedure(name = "getLocationDetails")
+    AddressDetails getLocationDetails(String regionName, String provinceName,
+                                      String municipalityName, String barangayName
+    );
+
+
 }
